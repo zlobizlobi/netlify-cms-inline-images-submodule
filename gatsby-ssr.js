b47b4436f47e1427d839eced0833e3e5bf200e2f
David Zlobinskyy <bureauzlobi@gmail.com>
2020-10-01 14:51:21 +0100

    fix: re-introduce categories collection and theme prop on ThemeProvider

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,7 +4,7 @@ import { MDX_SHORTCODES } from './src/cms/constants';
 import { MDXProvider } from '@mdx-js/react';
 
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>
+  <ThemeProvider theme={{}}>
     <MDXProvider components={MDX_SHORTCODES}>{element}</MDXProvider>
   </ThemeProvider>
 );
